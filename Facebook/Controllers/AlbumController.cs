2241f04e68b940c3619099da278df675e99a9a9c
Malaea Danut <dannycolours98@gmail.com>
2019-12-17 09:45:40 +0200

    added edit and delete for album

--- Facebook/Controllers/AlbumController.cs
+++ Facebook/Controllers/AlbumController.cs
@@ -66,5 +66,86 @@ namespace Facebook.Controllers
 			}
 		}
 
-    }
+		[Authorize(Roles = "Editor,Administrator")]
+		public ActionResult Edit(string id)
+		{
+			Album album = db.Albums.Find(id);
+			ViewBag.Album = album;
+			ApplicationUser user = db.Users.Find(album.user.Id);
+
+			if (album.userId == User.Identity.GetUserId() ||
+				User.IsInRole("Administrator"))
+			{
+				return View(album);
+			}
+			else
+			{
+				TempData["message"] = "Nu aveti dreptul sa faceti modificari asupra unui album care nu va apartine!";
+				return RedirectToAction("Index");
+			}
+
+		}
+
+		[HttpPut]
+		//[Authorize(Roles = "Editor,Administrator")]
+		public ActionResult Edit(string id, Album requestAlbum)
+		{
+
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					Album album = db.Albums.Find(id);
+					if (album.userId == User.Identity.GetUserId() ||
+						User.IsInRole("Administrator"))
+					{
+						if (TryUpdateModel(album))
+						{
+							album.name = requestAlbum.name;
+							db.SaveChanges();
+							TempData["message"] = "Albumul a fost modificat!";
+						}
+						return RedirectToAction("Index");
+					}
+					else
+					{
+						TempData["message"] = "Nu aveti dreptul sa faceti modificari asupra unui album care nu va apartine!";
+						return RedirectToAction("Index");
+					}
+
+				}
+				else
+				{
+					return View(requestAlbum);
+				}
+
+			}
+			catch (Exception e)
+			{
+				return View(requestAlbum);
+			}
+		}
+
+		[HttpDelete]
+		//[Authorize(Roles = "Editor,Administrator")]
+		public ActionResult Delete(string id)
+		{
+			Album album = db.Albums.Find(id);
+			if (album.userId == User.Identity.GetUserId() ||
+				User.IsInRole("Administrator"))
+			{
+				db.Albums.Remove(album);
+				db.SaveChanges();
+				TempData["message"] = "Albumul a fost sters!";
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				TempData["message"] = "Nu aveti dreptul sa stergeti un album care nu va apartine!";
+				return RedirectToAction("Index");
+			}
+
+		}
+
+	}
 }
